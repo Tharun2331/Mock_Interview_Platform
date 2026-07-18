26cdce79337e5b4c65cf2c554d22aeb5666daf28
Tharun2331 <tharunsd23@gmail.com>
2026-07-18 03:11:45 -0400

    Added SSM Parameter Store via Terraform

--- my-turborepo/infra/terraform/environments/dev/main.tf
+++ my-turborepo/infra/terraform/environments/dev/main.tf
@@ -3,3 +3,10 @@ module "iam" {
   environment = var.environment
   aws_region  = var.aws_region
 }
+
+module "ssm" {
+  source               = "../../modules/ssm"
+  environment          = var.environment
+  google_client_id     = var.google_client_id
+  google_client_secret = var.google_client_secret
+}
